6715e9340fe468dccfc7bf40197d80967f40044b
GabrielMSchmidt <gabrielmarochischmidt@gmail.com>
2023-07-05 10:45:37 -0300

    Começo da Listagem dos Usuários

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Gabriel Marochi',
+            'email' => 'gabriel@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+    }
+}

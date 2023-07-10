68a0235fefe14cad480cdf4ddb22dea6a923fd3e
GabrielMSchmidt <gabrielmarochischmidt@gmail.com>
2023-07-09 22:33:50 -0300

    Inicio do BD

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,1 +1,11 @@
-Listagem dos Usuarios
+<h1>Listagem dos Usuarios</h1>
+
+<ul>
+    @foreach($users as $user)
+        <li>
+            {{$user->name}} -
+            {{$user->email}}
+        </li>
+
+    @endforeach
+</ul>
